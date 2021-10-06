b71ab252ea8992ccfd10a9b1e2121259498c37fe
Atharva-Morankar-09 <90545519+Atharva-Morankar-09@users.noreply.github.com>
2021-10-06 16:08:52 +0530

    Update 3)Print Subarray with sum 0.cpp

--- 3)Print Subarray with sum 0.cpp
+++ 3)Print Subarray with sum 0.cpp
@@ -1,5 +1,57 @@
 //Print Subarrays having sum as 0
 
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+ 
+void printAllSubarrays(int arr[], int n)
+{
+    // create an empty multimap to store the ending index of all
+    // subarrays having the same sum
+    unordered_multimap<int, int> map;
+ 
+    // insert `(0, -1)` pair into the map to handle the case when
+    // subarray with zero-sum starts from index 0
+    map.insert(pair<int, int>(0, -1));
+ 
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        
+        sum += arr[i];
+ 
+        // if the sum is seen before, there exists at least one
+        // subarray with zero-sum
+        if (map.find(sum) != map.end())
+        {
+            auto it = map.find(sum);
+ 
+            // find all subarrays with the same sum
+            while (it != map.end() && it->first == sum)
+            {
+                cout << "Subarray [" << it->second + 1 << "..." << i << "]\n";
+                it++;
+            }
+        }
+ 
+        // insert (sum so far, current index) pair into multimap
+        map.insert(pair<int, int>(sum, i));
+    }
+}
+ 
+int main()
+{
+    int arr[] = { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    printAllSubarrays(arr, n);
+ 
+    return 0;
+}
+
+
+
 //  #include<iostream>
 //  using namespace std;
 
@@ -69,55 +121,3 @@
  
 //     return 0;
 // }
-
-#include <iostream>
-#include <unordered_map>
-using namespace std;
- 
-// Function to print all subarrays with a zero-sum in a given array
-void printAllSubarrays(int arr[], int n)
-{
-    // create an empty multimap to store the ending index of all
-    // subarrays having the same sum
-    unordered_multimap<int, int> map;
- 
-    // insert `(0, -1)` pair into the map to handle the case when
-    // subarray with zero-sum starts from index 0
-    map.insert(pair<int, int>(0, -1));
- 
-    int sum = 0;
- 
-    // traverse the given array
-    for (int i = 0; i < n; i++)
-    {
-        // sum of elements so far
-        sum += arr[i];
- 
-        // if the sum is seen before, there exists at least one
-        // subarray with zero-sum
-        if (map.find(sum) != map.end())
-        {
-            auto it = map.find(sum);
- 
-            // find all subarrays with the same sum
-            while (it != map.end() && it->first == sum)
-            {
-                cout << "Subarray [" << it->second + 1 << "..." << i << "]\n";
-                it++;
-            }
-        }
- 
-        // insert (sum so far, current index) pair into multimap
-        map.insert(pair<int, int>(sum, i));
-    }
-}
- 
-int main()
-{
-    int arr[] = { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
-    int n = sizeof(arr)/sizeof(arr[0]);
- 
-    printAllSubarrays(arr, n);
- 
-    return 0;
-}
